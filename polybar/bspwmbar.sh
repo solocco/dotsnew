72381876492e112b7a9eb42f3315aeecc1f67c7b
solocco <imron.turner@gmail.com>
2024-07-23 22:59:07 +0700

    Update and rename launch.sh to bspwmbar.sh

--- polybar/bspwmbar.sh
+++ polybar/bspwmbar.sh
@@ -9,6 +9,6 @@ killall -q polybar
 # Launch bar1 and bar2
 echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
 #polybar bar1 2>&1 | tee -a /tmp/polybar1.log & disown
-polybar example 2>&1 | tee -a /tmp/polybar2.log & disown
+polybar bspwmbar 2>&1 | tee -a /tmp/polybar2.log & disown
 
 echo "Bars launched..."
